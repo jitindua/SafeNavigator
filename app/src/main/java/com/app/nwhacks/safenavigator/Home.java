bbf1de8f4cff9c786fc46e1883cccbfcc0336bc5
Jitin Dua <jitindua@gmail.com>
2016-02-28 03:13:54 -0800

    Integrated new version

---
+++ app/src/main/java/com/app/nwhacks/safenavigator/Home.java
@@ -0,0 +1,137 @@
+package com.app.nwhacks.safenavigator;
+
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.content.Intent;
+/*
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.impl.client.DefaultHttpClient;*/
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+
+public class Home extends ActionBarActivity {
+     double latitude;
+     double longitute;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+
+           final EditText origin =(EditText)findViewById(R.id.origin);
+           final EditText destination =(EditText)findViewById(R.id.dest);
+           final Button go =(Button) findViewById(R.id.go);
+           go.setOnClickListener(new Button.OnClickListener(){
+                @Override
+                public void onClick(View v) {
+                    try {
+
+                        String org = origin.getText().toString();
+                        org.replace(" ", "+");
+                        String dest = destination.getText().toString();
+                        dest.replace(" ", "+");
+                        //Intent geo = new Intent(Intent.ACTION_VIEW, Uri.parse("geo:0,0?q=" + org));
+                        //startActivity(geo);
+                        //getLocationInfo(org);
+                        //getLocationInfo(dest);
+                        Intent map = new  Intent(Home.this, MapsActivity.class);
+                        map.putExtra("org",org);
+                        map.putExtra("dest",dest);
+                        startActivity(map);
+                    }catch(Exception e){
+                        //Log.e(TAG, e.getMessage());
+                    }
+                }
+
+        });
+    }
+/*
+    public JSONObject getLocationInfo(String address) {
+        StringBuilder stringBuilder = new StringBuilder();
+        try {
+
+            address = address.replaceAll(" ","%20");
+
+            HttpPost httppost = new HttpPost("http://maps.google.com/maps/api/geocode/json?address=" + address + "&sensor=false");
+            HttpClient client = new DefaultHttpClient();
+            HttpResponse response;
+            stringBuilder = new StringBuilder();
+
+            response = client.execute(httppost);
+            HttpEntity entity = response.getEntity();
+            InputStream stream = entity.getContent();
+            int b;
+            while ((b = stream.read()) != -1) {
+                stringBuilder.append((char) b);
+            }
+        } catch (ClientProtocolException e) {
+        } catch (IOException e) {
+        }
+
+        JSONObject jsonObject = new JSONObject();
+        try {
+            jsonObject = new JSONObject(stringBuilder.toString());
+        } catch (JSONException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+
+        return jsonObject;
+    }
+
+
+    public  boolean getLatLong(JSONObject jsonObject) {
+
+        try {
+            longitute = ((JSONArray)jsonObject.get("results")).getJSONObject(0)
+                    .getJSONObject("geometry").getJSONObject("location")
+                    .getDouble("lng");
+
+            latitude = ((JSONArray)jsonObject.get("results")).getJSONObject(0)
+                    .getJSONObject("geometry").getJSONObject("location")
+                    .getDouble("lat");
+
+        } catch (JSONException e) {
+            return false;
+
+        }
+
+        return true;
+    }
+*/
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_home, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
---
+++ app/src/main/java/com/app/nwhacks/safenavigator/MapsActivity.java
@@ -0,0 +1,214 @@
+package com.app.nwhacks.safenavigator;
+
+import android.os.AsyncTask;
+import android.support.v4.app.FragmentActivity;
+import android.os.Bundle;
+import android.util.Log;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+//import java.net.URLConnection;
+
+public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
+
+    private GoogleMap mMap;
+    private String org, dest;
+    private static final String LOG_TAG = "MAP";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_maps);
+        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+    }
+
+    protected void FindRoute(String origin, String Destination)
+    {
+
+
+    }
+
+
+    /**
+     * Manipulates the map once available.
+     * This callback is triggered when the map is ready to be used.
+     * This is where we can add markers or lines, add listeners or move the camera. In this case,
+     * we just add a marker near Sydney, Australia.
+     * If Google Play services is not installed on the device, the user will be prompted to install
+     * it inside the SupportMapFragment. This method will only be triggered once the user has
+     * installed Google Play services and returned to the app.
+     */
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+
+
+        Bundle extras = getIntent().getExtras();
+        if (extras != null) {
+            org = extras.getString("org");
+            dest = extras.getString("dest");
+        }
+        String YOUR_API_KEY = this.getString(R.string.google_maps_key);
+        org = org.replaceAll(" ","%20");
+        dest = dest.replaceAll(" ","%20");
+        //String route = "https://maps.googleapis.com/maps/api/directions/json?origin="+org+"&destination="+dest+"&key="+YOUR_API_KEY;
+        String route = "https://maps.googleapis.com/maps/api/directions/json?alternatives=true&origin="+org+"&destination="+dest;
+        //route = "https://maps.googleapis.com/maps/api/directions/json?origin=Toronto&destination=Montreal";//&key="+"AIzaSyDweSI2IGmUOj80B22rk0oExx7MeLQ5LRs";
+        //JSONObject jsonObject = getLocationInfo(route);
+        //RetrieveFeedTask temp = new RetrieveFeedTask();
+        JSONObject jsonObject = null;
+        try {
+
+                jsonObject = new RetrieveFeedTask().execute(route).get();
+            ;
+        }catch (Exception e){
+            Log.i(LOG_TAG, e.toString());
+        }
+        if(jsonObject == null)
+        {
+            Log.i(LOG_TAG,"NULL json 1");
+            Log.i(LOG_TAG,YOUR_API_KEY);
+        }
+        else {
+
+            Log.i(LOG_TAG, "JSON Size:\n" + jsonObject.toString());
+        }
+
+        // Add a marker in Sydney and move the camera
+        /*
+        LatLng sydney = new LatLng(-34, 151);
+        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+        */
+        //mMap.
+    }
+
+    public JSONObject getLocationInfo(String route) {
+        StringBuffer buffer = new StringBuffer();
+        try {
+            long currentTime = System.currentTimeMillis();
+            //URL url = new URL("http://maps.google.com/maps/api/geocode/json?address=" + "Delhi" + "&sensor=false");
+            URL url = new URL(route);
+           //URL url = new URL("https://maps.googleapis.com/maps/api/geocode/json?address=" + "Delhi" +"&key=" + this.getString(R.string.google_maps_key));
+            //URL url = new URL("https://maps.googleapis.com/maps/api/geocode/json?address=" + "Delhi");
+
+            HttpURLConnection connection = (HttpURLConnection)url.openConnection();
+            connection.setRequestMethod("GET");
+            connection.connect();
+
+            int responseCode = connection.getResponseCode();
+            if (responseCode == HttpURLConnection.HTTP_OK) {
+
+                BufferedReader reader = null;
+
+                InputStream inputStream = connection.getInputStream();
+                //buffer = new StringBuffer();
+                if (inputStream == null) {
+                    // Nothing to do.
+                    Log.i(LOG_TAG, "NULL 1");
+                    return null;
+                }
+                reader = new BufferedReader(new InputStreamReader(inputStream));
+
+                String line;
+                while ((line = reader.readLine()) != null) {
+                    long elapsedTime = System.currentTimeMillis();
+                    if(elapsedTime-currentTime>=5000) {
+                        Log.i(LOG_TAG, "NULL 2");
+                        return null;
+                    }
+                    buffer.append(line + "\n");
+                }
+
+                Log.i(LOG_TAG, buffer.toString());
+
+                if (buffer.length() == 0) {
+                    Log.i(LOG_TAG, "NULL 3");
+                    return null;
+                }
+
+                //Log.d("Test", buffer.toString());
+                //return buffer.toString();
+            }
+            else {
+                //Log.i(TAG, "Unsuccessful HTTP Response Code: " + responseCode);
+                Log.i(LOG_TAG, "NULL 4");
+                return null;
+            }
+        } catch (MalformedURLException e) {
+            Log.i(LOG_TAG, "NULL 5");
+            return null;
+        } catch (IOException e) {
+            Log.i(LOG_TAG, "NULL 6");
+            return null;
+        } catch (Exception e) {
+            Log.i(LOG_TAG, "NULL 7" + e.toString());
+            return null;
+        }
+
+
+        //return false;
+        JSONObject jsonObject = new JSONObject();
+        try {
+            jsonObject = new JSONObject(buffer.toString());
+        } catch (JSONException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+            Log.i(LOG_TAG, "NULL 8");
+            return  null;
+        }
+        if(jsonObject!=null)
+             Log.i(LOG_TAG, "Not null");
+        else {
+            Log.i(LOG_TAG, "NULL 9");
+        }
+        Log.i(LOG_TAG, "NULL 10");
+
+        return jsonObject;
+    }
+
+
+
+
+
+    class RetrieveFeedTask extends AsyncTask<String, Void, JSONObject> {
+
+        private Exception exception;
+
+        protected JSONObject doInBackground(String... urls) {
+            try {
+                return getLocationInfo(urls[0]);
+            } catch (Exception e) {
+                this.exception = e;
+                return null;
+            }
+        }
+
+        protected void onPostExecute(JSONObject json) {
+            // TODO: check this.exception
+            // TODO: do something with the feed
+        }
+    }
+
+}
+
+
+
